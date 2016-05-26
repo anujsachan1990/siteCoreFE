ccb126d250e10ffaefff3b01aef45662b1444a05
Anuj Sachan <asachan@sapient.com>
2016-05-26 15:34:35 +0530

    added tasks for ssh deployments

---
+++ grunt/options/environments.js
@@ -0,0 +1,42 @@
+module.exports = {
+  // do not store credentials in the git repo, store them separately and read from a secret file
+  secret: {
+    "staging": {
+        "host": "staging-host",
+        "username": "",
+        "password": "",
+        "port": "22"
+    },
+    "production": {
+        "host": "production-host",
+        "username": "",
+        "password": "",
+        "port": "22"
+    }
+},
+options: {
+  local_path: 'dist',
+  current_symlink: 'current',
+  deploy_path: '/full/path'
+},
+staging: {
+    options: {
+        host: '<%= secret.staging.host %>',
+        username: '<%= secret.staging.username %>',
+        password: '<%= secret.staging.password %>',
+        port: '<%= secret.staging.port %>',
+        debug: true,
+        releases_to_keep: '3'
+    }
+},
+production: {
+    options: {
+        host: '<%= secret.production.host %>',
+        username: '<%= secret.production.username %>',
+        password: '<%= secret.production.password %>',
+        port: '<%= secret.production.port %>',
+        releases_to_keep: '5',
+        release_subdir: 'myapp'
+    }
+}
+}
