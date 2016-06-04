9809c614f43604d3620bf6745658e3ffbc5cd64f
Anuj Sachan <asachan@sapient.com>
2016-06-04 19:30:53 +0530

    grunt build

--- dist/js/main.js
+++ dist/js/main.js
@@ -743,6 +743,68 @@ jQuery(sunSpr.Header.init());
 var sunSpr = window.sunSpr || {};
 
 /**
+ * @namespace SampleModule
+ * @memberof sunSpr
+ * @property {null} property - description of property
+ */
+ 
+sunSpr.SampleModule = (function (window, $, namespace) {
+    'use strict';
+
+    // public methods
+    var init,
+
+    // private methods
+        _privateMethod,
+
+    // properties
+        property = null;
+
+    /**
+     * @method _privateMethod
+     * @description Description of _privateMethod
+     * @memberof sunSpr.SampleModule
+     */
+    _privateMethod = function () {
+        return property;
+    };
+
+    /**
+     * @method init
+     * @description Description of init
+     * @memberof sunSpr.SampleModule
+     * @example
+     * sunSpr.SampleModule.init()
+     */
+    init = function () {
+        return _privateMethod();
+    };
+
+    // Public API
+    return {
+        init: init
+    };
+
+}(this, jQuery, 'sunSpr'));
+
+jQuery(sunSpr.SampleModule.init());
+;/*jslint forin: true, sloppy: true, unparam: true, vars: true, white: true, nomen: true, plusplus:true */
+/*global window, document, jQuery, console */
+
+/*
+ * sample-module.js
+ * [ Description of the sample module script ]
+ *
+ * @project:    SunSuper
+ * @date:       xxxx-xx-xx
+ * @author:     Anuj Sachan, asachan@sapient.com
+ * @licensor:   SAPIENNITRO
+ * @namespaces: sunSpr
+ */
+
+var sunSpr = window.sunSpr || {};
+
+/**
  * @namespace Nav
  * @memberof sunSpr
  * @property {null} property - description of property
